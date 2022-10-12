68f78c394527ad01b659a8fca2472cdd7af275e5
darknight <darkniuslightness@gmail.com>
2022-10-12 20:57:35 +0700

    Spoof fingerprint for Google Play Services and SafetyNet

--- config/common.mk
+++ config/common.mk
@@ -151,3 +151,10 @@ DEVICE_PACKAGE_OVERLAYS += vendor/nusantara/overlay/common
 
 # QTI Components
 $(call inherit-product-if-exists, device/qcom/common/common.mk)
+
+# Spoof fingerprint for Google Play Services and SafetyNet
+ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
+endif
