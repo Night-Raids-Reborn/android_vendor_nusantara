Night-Raids-Reborn/platform_vendor_bliss

[SQUASH] vendor: Toggle-able adblock hosts file [2/3]
Regenerate with ./adaway/generate.sh

Change-Id: Ia389457746f9af1060a1c1ae62af0cb9ca643a71

vendor: Add StevenBlack source

Change-Id: I89eedb7ad2a34b769b0a029dd1e29893a157f24f

hosts.aicp_adblock: sync from git, update on build [2/2]

Change-Id: I5d9ad3307f7137879c407791fb6cd66c1463f4cf

vendor: whitelist some google adservices

They spam logcat as hell!

Change-Id: I0e4abde69615ce6f297bf52f80fa4e7ce5277b1d

aicp: adblock: Allow overriding the input hosts file

Because of the modular nature of the StevenBlack's hosts, the user may be tempted to modify the hosts to match personal preferences. Accommodate such changes.
Retains the earlier behaviour to pass multiple files to be conbined.

Also remove the shell script "middleman", earlier shell script was used to fetch the file versions, which now is handled within the python script itself

Change-Id: I4a348f29c9dbec6eb9eafe387a14be7280ce2412

adaway: Minor improvements

- Sync up custom host file with stock AOSP hosts
- Use assigment operator rather than a 'if' block in the Makefile for custom hosts

Change-Id: I21dc649f5de9d2ab1c1132093f10ed2486b9d1e2

adaway: Whitelist a Youtube subdomain

Fixes: Watch history not working on YT Vanced

Ref: https://github.com/AdAway/AdAway/issues/2071

Change-Id: I376f249d9da0db61506858890f8e4e9cb2c4aaa6

fix path

adaway/Android.mk
@@ -1,0 +1,18 @@
+# Use 'export INPUT_ADBLOCK_FILE=<custom host file location>' before build/envsetup.sh to build custom adblock
+INPUT_ADBLOCK_FILE ?= external/hosts/hosts
+
+ADBLOCK_SCRIPT := vendor/bliss/adaway/generate.py
+
+TARGET_GENERATED_HOSTS_BLOCKER := $(TARGET_OUT_INTERMEDIATES)/ADBLOCK/hosts.aicp_adblock
+$(TARGET_GENERATED_HOSTS_BLOCKER): INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/ADBLOCK
+$(TARGET_GENERATED_HOSTS_BLOCKER): $(ADBLOCK_SCRIPT) $(INPUT_ADBLOCK_FILE)
+	python3 $(ADBLOCK_SCRIPT) $@ $(INPUT_ADBLOCK_FILE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hosts.aicp_adblock
+LOCAL_MODULE_CLASS := ETC
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): $(TARGET_GENERATED_HOSTS_BLOCKER)
+	@cp $< $@

Night-Raids-Reborn/android_vendor_nusantara

adaway/Android.mk
@@ -1,0 +1,18 @@
+# Use 'export INPUT_ADBLOCK_FILE=<custom host file location>' before build/envsetup.sh to build custom adblock
+INPUT_ADBLOCK_FILE ?= external/hosts/hosts
+
+ADBLOCK_SCRIPT := vendor/fluid/adaway/generate.py
+
+TARGET_GENERATED_HOSTS_BLOCKER := $(TARGET_OUT_INTERMEDIATES)/ADBLOCK/hosts.aicp_adblock
+$(TARGET_GENERATED_HOSTS_BLOCKER): INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/ADBLOCK
+$(TARGET_GENERATED_HOSTS_BLOCKER): $(ADBLOCK_SCRIPT) $(INPUT_ADBLOCK_FILE)
+	python3 $(ADBLOCK_SCRIPT) $@ $(INPUT_ADBLOCK_FILE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hosts.aicp_adblock
+LOCAL_MODULE_CLASS := ETC
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): $(TARGET_GENERATED_HOSTS_BLOCKER)
+	@cp $< $@